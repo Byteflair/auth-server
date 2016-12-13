6f090dcb6fd1eab8214548d3e1467e3f27a8b044
calata <carlos.calatayud@byteflair.com>
2016-12-13 12:56:59 +0100

    Stop loggerContext al cerrar aplicación (memory leaks)

--- oauth2-server-common/src/main/java/com/byteflair/oauth/server/OauthServerConfig.java
+++ oauth2-server-common/src/main/java/com/byteflair/oauth/server/OauthServerConfig.java
@@ -1,10 +1,14 @@
 package com.byteflair.oauth.server;
 
+import ch.qos.logback.classic.LoggerContext;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+
+import javax.annotation.PreDestroy;
 
 /**
  * Created by calata on 15/11/16.
@@ -19,4 +23,10 @@ import org.springframework.context.annotation.Import;
 @EnableAutoConfiguration
 @SpringBootConfiguration
 public class OauthServerConfig {
+
+    @PreDestroy
+    public void onShutdown() {
+        LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
+        loggerContext.stop();
+    }
 }
