b96cd58bff21c8b8830311fb998244ddcc6d3b9c
calata <carlos.calatayud@byteflair.com>
2017-01-17 16:04:36 +0100

    Lógica negocio /client a CustomClientDetailsService access_token_validity y refresh_token_validity en application.yml

--- oauth2-server-common/src/main/java/com/byteflair/oauth/server/controllers/ClientController.java
+++ oauth2-server-common/src/main/java/com/byteflair/oauth/server/controllers/ClientController.java
@@ -1,18 +1,10 @@
 package com.byteflair.oauth.server.controllers;
 
+import com.byteflair.oauth.server.service.CustomClientDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.provider.ClientDetails;
 import org.springframework.security.oauth2.provider.client.BaseClientDetails;
-import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
-import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -21,74 +13,30 @@ import java.util.List;
 @RestController
 public class ClientController {
 
-    private static final Integer ACCESS_TOKEN_VALIDITY = 900;
-    private static final Integer REFRESH_TOKEN_VALIDITY = 43200;
-
     @Autowired
-    @Qualifier("jdbcClientDetailsService")
-    JdbcClientDetailsService jdbcClientDetailsService;
-
-    @Autowired
-    PasswordEncoder passwordEncoder;
+    private CustomClientDetailsService clientDetailsService;
 
     @RequestMapping(method = RequestMethod.POST, value = "/client")
     public BaseClientDetails createNewClient(@RequestBody BaseClientDetails clientDetails) {
 
-        Assert.notNull(clientDetails);
-        Assert.hasText(clientDetails.getClientId(), "client_id must be informed");
-        Assert.hasText(clientDetails.getClientSecret(), "client_secret must be informed");
-        Assert.isTrue(!clientDetails.getScope().isEmpty(), "at least one scope must be informed");
+        BaseClientDetails createdClientDetail = clientDetailsService.createClientDetails(clientDetails);
 
-        if (clientDetails.getAuthorities() == null || clientDetails.getAuthorities().isEmpty()) {
-            GrantedAuthority authority = new SimpleGrantedAuthority("ROLE_TRUSTED_CLIENT");
-            clientDetails.setAuthorities(Arrays.asList(authority));
-        }
-
-        if (clientDetails.getAuthorizedGrantTypes() == null || clientDetails.getAuthorizedGrantTypes().isEmpty()) {
-            clientDetails.setAuthorizedGrantTypes(Arrays.asList("client_credentials"));
-        }
-
-        if (clientDetails.getAccessTokenValiditySeconds() == null) {
-            clientDetails.setAccessTokenValiditySeconds(ACCESS_TOKEN_VALIDITY);
-        }
-
-        if (clientDetails.getRefreshTokenValiditySeconds() == null) {
-            clientDetails.setRefreshTokenValiditySeconds(REFRESH_TOKEN_VALIDITY);
-        }
-
-        jdbcClientDetailsService.setPasswordEncoder(passwordEncoder);
-        jdbcClientDetailsService.addClientDetails(clientDetails);
-
-        clientDetails = (BaseClientDetails) jdbcClientDetailsService.loadClientByClientId(clientDetails.getClientId());
-        clientDetails.setClientSecret("*");
-        return clientDetails;
+        return createdClientDetail;
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/client/{id}")
     public BaseClientDetails getClientDetails(@PathVariable(value = "id") String id){
 
-        BaseClientDetails clientDetails = null;
-        Assert.hasText(id, "id must be informed");
-
-        clientDetails = (BaseClientDetails) jdbcClientDetailsService.loadClientByClientId(id);
-        clientDetails.setClientSecret("*");
+        BaseClientDetails clientDetails = clientDetailsService.getClientDetails(id);
 
         return clientDetails;
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/client")
     public List<BaseClientDetails> getAllClientDetails() {
-        List<BaseClientDetails> baseClientDetails = new ArrayList<>();
-        List<ClientDetails> clientDetailsList = jdbcClientDetailsService.listClientDetails();
-
-        for (ClientDetails clientDetails: clientDetailsList){
-            BaseClientDetails baseClient = (BaseClientDetails) clientDetails;
-            baseClient.setClientSecret("*");
-            baseClientDetails.add(baseClient);
-        }
+        List<BaseClientDetails> baseClientDetails = clientDetailsService.getAllClientDetails();
 
         return baseClientDetails;
-
     }
 
     /*@RequestMapping(method = RequestMethod.DELETE, value = "/client/{id}")
---
+++ oauth2-server-common/src/main/java/com/byteflair/oauth/server/service/CustomClientDetailsService.java
@@ -0,0 +1,93 @@
+package com.byteflair.oauth.server.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.provider.ClientDetails;
+import org.springframework.security.oauth2.provider.client.BaseClientDetails;
+import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
+import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by calata on 17/01/17.
+ */
+@Service
+public class CustomClientDetailsService {
+
+    @Value("${api.client.access_token_validity}")
+    private Integer defaultAccessTokenValidity;
+    @Value("${api.client.refresh_token_validity}")
+    private Integer defaultRefreshTokenValidity;
+
+    @Autowired
+    @Qualifier("jdbcClientDetailsService")
+    private JdbcClientDetailsService jdbcClientDetailsService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public BaseClientDetails createClientDetails(BaseClientDetails clientDetails) {
+
+        Assert.notNull(clientDetails);
+        Assert.hasText(clientDetails.getClientId(), "client_id must be informed");
+        Assert.hasText(clientDetails.getClientSecret(), "client_secret must be informed");
+        Assert.isTrue(!clientDetails.getScope().isEmpty(), "at least one scope must be informed");
+
+        if (clientDetails.getAuthorities() == null || clientDetails.getAuthorities().isEmpty()) {
+            GrantedAuthority authority = new SimpleGrantedAuthority("ROLE_TRUSTED_CLIENT");
+            clientDetails.setAuthorities(Arrays.asList(authority));
+        }
+
+        if (clientDetails.getAuthorizedGrantTypes() == null || clientDetails.getAuthorizedGrantTypes().isEmpty()) {
+            clientDetails.setAuthorizedGrantTypes(Arrays.asList("client_credentials"));
+        }
+
+        if (clientDetails.getAccessTokenValiditySeconds() == null) {
+            clientDetails.setAccessTokenValiditySeconds(defaultAccessTokenValidity);
+        }
+
+        if (clientDetails.getRefreshTokenValiditySeconds() == null) {
+            clientDetails.setRefreshTokenValiditySeconds(defaultRefreshTokenValidity);
+        }
+
+        jdbcClientDetailsService.setPasswordEncoder(passwordEncoder);
+        jdbcClientDetailsService.addClientDetails(clientDetails);
+
+        clientDetails = getClientDetails(clientDetails.getClientId());
+
+        return clientDetails;
+    }
+
+    public BaseClientDetails getClientDetails(String id) {
+        BaseClientDetails clientDetails = null;
+
+        Assert.hasText(id, "id must be informed");
+
+        clientDetails = (BaseClientDetails) jdbcClientDetailsService.loadClientByClientId(id);
+        clientDetails.setClientSecret("*****");
+
+        return clientDetails;
+    }
+
+    public List<BaseClientDetails> getAllClientDetails() {
+
+        List<BaseClientDetails> baseClientDetails = new ArrayList<>();
+        List<ClientDetails> clientDetailsList = jdbcClientDetailsService.listClientDetails();
+
+        for (ClientDetails clientDetails : clientDetailsList) {
+            BaseClientDetails baseClient = (BaseClientDetails) clientDetails;
+            baseClient.setClientSecret("*****");
+            baseClientDetails.add(baseClient);
+        }
+
+        return baseClientDetails;
+    }
+}
