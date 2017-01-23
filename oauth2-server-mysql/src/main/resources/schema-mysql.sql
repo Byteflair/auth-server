b2e2a47c117118179a070682372824acac270fd5
calata <carlos.calatayud@byteflair.com>
2017-01-23 15:33:27 +0100

    Eliminación tablas JDBC Token Store

--- oauth2-server-mysql/src/main/resources/schema-mysql.sql
+++ oauth2-server-mysql/src/main/resources/schema-mysql.sql
@@ -4,13 +4,7 @@ CREATE DATABASE IF NOT EXISTS `oauth_db`;
 DROP TABLE IF EXISTS `rolesperuser`;
 DROP TABLE IF EXISTS `rolespergroup`;
 DROP TABLE IF EXISTS `roles`;
-DROP TABLE IF EXISTS `persistenlogins`;
-DROP TABLE IF EXISTS `oauth_refresh_token`;
-DROP TABLE IF EXISTS `oauth_code`;
-DROP TABLE IF EXISTS `oauth_client_token`;
 DROP TABLE IF EXISTS `oauth_client_details`;
-DROP TABLE IF EXISTS `oauth_approvals`;
-DROP TABLE IF EXISTS `oauth_access_token`;
 DROP TABLE IF EXISTS `groupmembers`;
 DROP TABLE IF EXISTS `userdetails`;
 DROP TABLE IF EXISTS `users`;
@@ -65,26 +59,6 @@ CREATE TABLE `groupmembers` (
   CONSTRAINT `userstogroupmembers` FOREIGN KEY (`iduser`) REFERENCES `users` (`iduser`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `oauth_access_token` (
-  `token_id` varchar(255) DEFAULT NULL,
-  `token` mediumblob,
-  `authentication_id` varchar(255) NOT NULL,
-  `user_name` varchar(255) DEFAULT NULL,
-  `client_id` varchar(255) DEFAULT NULL,
-  `authentication` mediumblob,
-  `refresh_token` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`authentication_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE `oauth_approvals` (
-  `userId` varchar(255) DEFAULT NULL,
-  `clientId` varchar(255) DEFAULT NULL,
-  `scope` varchar(255) DEFAULT NULL,
-  `status` varchar(10) DEFAULT NULL,
-  `expiresAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `lastModifiedAt` timestamp NOT NULL DEFAULT '1970-01-01 00:00:01'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 CREATE TABLE `oauth_client_details` (
   `client_id` varchar(255) NOT NULL,
   `resource_ids` varchar(255) DEFAULT NULL,
@@ -98,34 +72,6 @@ CREATE TABLE `oauth_client_details` (
   `additional_information` varchar(4096) DEFAULT NULL,
   `autoapprove` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`client_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE `oauth_client_token` (
-  `token_id` varchar(255) DEFAULT NULL,
-  `token` mediumblob,
-  `authentication_id` varchar(255) NOT NULL,
-  `user_name` varchar(255) DEFAULT NULL,
-  `client_id` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`authentication_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE `oauth_code` (
-  `code` varchar(255) DEFAULT NULL,
-  `authentication` mediumblob
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE `oauth_refresh_token` (
-  `token_id` varchar(255) DEFAULT NULL,
-  `token` mediumblob,
-  `authentication` mediumblob
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE `persistenlogins` (
-  `username` varchar(64) CHARACTER SET latin1 NOT NULL,
-  `series` varchar(64) CHARACTER SET latin1 NOT NULL,
-  `token` varchar(64) CHARACTER SET latin1 NOT NULL,
-  `lastused` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`series`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `roles` (
